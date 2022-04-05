78ea5d8edb016264a8490d82a950fcc3861a6667
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-04-05 19:35:02 +0100

    display_sum_of_prices_when_total_is_introduced_when_scan_23456_barcode

--- PointOfSale/PointOfSale/PoinOfSaleSpecs.cs
+++ PointOfSale/PointOfSale/PoinOfSaleSpecs.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using NUnit.Framework;
 
 namespace PointOfSaleSpecs {
@@ -31,12 +32,32 @@ namespace PointOfSaleSpecs {
             Assert.AreEqual("Error: barcode not found", price);
         }
 
+        [Test]
+        public void display_barcode_empty_message_when_scan_empty_barcode() {
+            var price = string.Empty;
+
+            price = PointOfSale.Run("");
+
+            Assert.AreEqual("Error: empty barcode", price);
+        }
+
+        [Test]
+        public void display_sum_of_prices_when_total_is_introduced_when_scan_23456_barcode() {
+            var price = string.Empty;
+
+            price = PointOfSale.Run("23456", 2);
+
+            Assert.AreEqual("$25.00", price);
+        }
+
     }
 
     public class PointOfSale {
-        public static string Run(string barCode) {
+
+        public static string Run(string barCode, int total = 1) {
+            if (barCode == string.Empty) return "Error: empty barcode";
             if (barCode == "99999") return "Error: barcode not found";
-            if (barCode == "23456") return "$12.50";
+            if (barCode == "23456") return $"${(12.50D * total).ToString("0.00", CultureInfo.InvariantCulture)}";
             return "$7.25";
         }
     }
