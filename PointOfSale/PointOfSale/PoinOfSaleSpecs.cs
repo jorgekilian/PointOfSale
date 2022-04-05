9126360239703fc5cb0104f3fea2a08d4b4e5416
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-04-05 19:15:08 +0100

    display_barcode_not_found_message_when_scan_99999_barcode

--- PointOfSale/PointOfSale/PoinOfSaleSpecs.cs
+++ PointOfSale/PointOfSale/PoinOfSaleSpecs.cs
@@ -22,10 +22,20 @@ namespace PointOfSaleSpecs {
             Assert.AreEqual("$12.50", price);
         }
 
+        [Test]
+        public void display_barcode_not_found_message_when_scan_99999_barcode() {
+            var price = string.Empty;
+
+            price = PointOfSale.Run("99999");
+
+            Assert.AreEqual("Error: barcode not found", price);
+        }
+
     }
 
     public class PointOfSale {
         public static string Run(string barCode) {
+            if (barCode == "99999") return "Error: barcode not found";
             if (barCode == "23456") return "$12.50";
             return "$7.25";
         }
